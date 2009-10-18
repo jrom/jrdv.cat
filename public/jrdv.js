bafc40328de21ccca17572cdb9e658ac48c9f118
Jordi Romero <jordi@jrom.net>
2009-10-18 16:15:55 +0200

    Adding static assets

---
+++ public/jrdv.js
@@ -0,0 +1,18 @@
+function hivelogic_enkoder(){var kode=
+"kode=\")''(nioj.)(esrever.)''(tilps.edok=edok;\\\"kode=\\\"\\\\oked\\\\\\"+
+"\\\\\"\\\\=')('injo).e(rsvere).''t(lispe.od=kdeko\\\\\\\\\\\\\\\\;k\\\\\\"+
+"\\\\\"\\\\do=e\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"+
+"\\\\=xdeko)}(cdeCoarChomfrg.intr=Sx+8;12+=)c<0(cif3;)-(iAtdeCoarche.od=k{c"+
+"+)i+h;gten.ldekoi<0;i=r(fo';=';x\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"+
+"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"+
+"\\\\\\\\\\\\*,+*lqmr,1h+uvyhuh,1**w+olvsh1rg@nghnr%>rgxfhpwqz1luhw%+?_#duk"+
+"ih_@%_p_ldwo=rqlrimCgu1ydf_w%_#_lwow@h___%qlrimCgu1ydf_w%_A_qlrimCgu1ydf?w"+
+"d2%A,_%>h@rg\\\\\\\\\\\\\\\\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"+
+"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\=\\\\\\\\\\\\\\\\deko"+
+"\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\k;do=eoked"+
+"s.lpti'()'r.verees)(j.io(n''\\\\\\\\\\\\\\\\)=\\\\\\\\\\\"\\\\deko;\\\\\\"+
+"\\\\\"\\\\okedk=do.epsil(t''.)erevsr(e.)ojni'()'\\\"\\\\;x='';for(i=0;i<(k"+
+"ode.length-1);i+=2){x+=kode.charAt(i+1)+kode.charAt(i)}kode=x+(i<kode.leng"+
+"th?kode.charAt(kode.length-1):'');\\\"=edok\";kode=kode.split('').reverse("+
+").join('')"
+;var i,c,x;while(eval(kode));}
