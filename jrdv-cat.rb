d5f776de3f1710984d88563d032f6010ebaebdd3
Jordi Romero <jordi@jrom.net>
2009-10-18 17:30:28 +0200

    Content dumped and nearly working

--- jrdv-cat.rb
+++ jrdv-cat.rb
@@ -1,7 +1,29 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/r18n'
+
+before do
+
+  case env['SERVER_NAME']
+  when "jrdv.cat"
+    @lang = 'ca'
+  when "jrdv.es"
+    @lang = 'es'
+  when "jrdv.org"
+    @lang = 'en'
+  else
+    @lang = 'ca'
+  end
+
+  session[:locale] = @lang
+end
 
 get '/' do
-  @lang = 'ca'
+  @classecos = 'inici'
   erb :index
+end
+
+get %r{/(treballs|trabajos|works)} do
+  @classecos = ''
+  erb :treballs
 end
