cfdb580b4b7623d8f419d6a22086132969f21a70
Jordi Romero <jordi@jrom.net>
2009-10-19 14:15:32 +0200

    First steps to dynamic version... content in DB

--- jrdv-cat.rb
+++ jrdv-cat.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
 require 'sinatra/r18n'
+require 'dm-core'
+require 'dm-timestamps'
+
+# DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://{#Dir.pwd}/info.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/adserver.db")
+
+class Page
+  include DataMapper::Resource
+
+  property :id,           Serial
+  property :url,          String
+  property :title,        String
+  property :lang,         String
+  property :content,      Text
+  property :created_at,   DateTime
+  property :updated_at,   DateTime
+end
+
+DataMapper.auto_upgrade!
 
 before do
 
@@ -25,4 +44,26 @@ end
 get %r{/(treballs|trabajos|works)} do
   @classecos = ''
   erb :treballs
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/create' do
+  @page = Page.new(params[:page])
+  if @page.save
+    redirect("/#{@page.url}")
+  else
+    redirect('/new')
+  end
+end
+
+get '/*' do
+  @page = Page.first(:url => params[:splat])
+  if @page
+    erb(@page.content)
+  else
+    erb("Not Found")
+  end
 end
